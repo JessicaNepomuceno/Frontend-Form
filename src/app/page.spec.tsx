21efe8e0b9e2e2fe3c749562f6c66e78e6bf4fea
Jessica Nepomuceno <jessica_alves_nep@hotmail.com>
2024-10-20 13:08:19 -0400

    deleted all files

--- src/app/page.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render} from "@testing-library/react";
-import Home from "./page";
-import "@testing-library/jest-dom";
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Home />);
-  const linkElement = getByText(/Deploy now/i);
-  expect(linkElement).toBeInTheDocument();
-});
